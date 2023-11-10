8a28d6cd852b4227a3e82ec4f6da236b29451fbb
markiep <purcellmarkie@gmail.com>
2023-11-09 16:25:20 -0800

    init

---
+++ setenv.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# set Vieb envvars, listed in the 'vieb --help' cmd
+set -a
+
+VIEB_DEVTOOLS_THEME="dark"
+VIEB_DATAFOLDER="$XDG_DATA_HOME/Vieb"
+VIEB_ERWIC="$XDG_CONFIG_HOME/erwic.json"
+VIEB_AUTOPLAY_MEDIA="user"
+VIEB_ACCELERATION="hardware"
+
