76fc2144e43a11f4cc89305b8547bc4124d0f52c
markiep <purcellmarkie@gmail.com>
2024-05-13 18:12:17 -0700

    update

--- setenv.sh
+++ setenv.sh
@@ -2,9 +2,11 @@
 # set Vieb envvars, listed in the 'vieb --help' cmd
 set -a
 
-VIEB_DEVTOOLS_THEME="dark"
 VIEB_DATAFOLDER="$XDG_DATA_HOME/Vieb"
-VIEB_ERWIC="$XDG_CONFIG_HOME/erwic.json"
+#VIEB_ERWIC="$XDG_CONFIG_HOME/erwic.json"
+VIEB_ERWIC=""
+
+VIEB_DEVTOOLS_THEME="dark"
 VIEB_AUTOPLAY_MEDIA="user"
 VIEB_ACCELERATION="hardware"
 
